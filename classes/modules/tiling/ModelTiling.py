e2617a60c50c52c20fc47db6e72aabccc0f2b5c6
alek9z <alessandro.zangari.code@outlook.com>
2022-11-29 09:12:10 +0100

    Tiling module with optional resize of output and better parameters + docs

--- classes/modules/tiling/ModelTiling.py
+++ classes/modules/tiling/ModelTiling.py
@@ -7,31 +7,49 @@ from classes.core.Model import Model
 
 
 class ModelTiling(Model):
-    _default_parameters = dict(kernel_size=(64, 64), dilation=1, padding=0, stride=64, device="cuda")
+    """
+    Image tile extractor: extract patches from batch of images and stack them together.
+    Look at https://pytorch.org/docs/stable/generated/torch.nn.Unfold.html for a list of accepted "network_params".
 
-    def __init__(self, network_params: Dict = _default_parameters):
-        super().__init__(device=network_params.pop("device"))
+    The "resize" parameter controls whether patches are stacked as 3D tensors (true) or flattened in a 1D tensor (false).
+    Look at the :py:meth:`.ModelTiling.predict`.
+    """
+    _default_parameters = dict(kernel_size=(64, 64), dilation=1, padding=0, stride=64)
+
+    def __init__(self, device: torch.device,
+                 network_params: Dict = _default_parameters,
+                 resize: bool = False):
+        super().__init__(device=device)
         self._network = Unfold(**network_params).to(self._device)
+        self.__resize: bool = resize
 
     def predict(self, x: torch.Tensor, *args: any, **kwargs: any) -> torch.Tensor:
         """
         Tile a batch of 2D images into patches
 
         :param x: 4D tensor (batch, channels, height, width)
-        :param args:
-        :param kwargs:
-        :return: 3D tensor (batch, features, n_patches). "features" is given by (kernel_size * channels)
+        :return: If "resize" is False a 3D tensor (batch, n_patches, features). "features" is given by (patch_height * patch_width * channels)
+            If "resize" is True a 5D tensor as (batch, n_patches, channels, patch_height, patch_width)
         """
 
-        return self._network(x)
+        # Batch and channels num are preserved in output
+        b, c, *_ = x.size()
+        # Get the patches dimensions
+        ph, pw = self._network.kernel_size
+        # Extract patches
+        o = self._network(x).transpose(1, 2)
+        # Reshape if needed
+        if self.__resize:
+            return o.reshape(b, -1, c, ph, pw)
+        return o
 
 # Example of usage
 # if __name__ == '__main__':
-#     m = ModelTiling()
+#     m = ModelTiling(device="cuda", resize=True)
 #     # m = ModelSLIC()
 #
-#     x = torch.randn((8, 3, 128, 128))
+#     x = torch.randn((8, 3, 256, 256))
 #
 #     # xs = m.predict(x.transpose(1, -1).numpy())
-#     xs = m.predict()
+#     xs = m.predict(x)
 #     pass
