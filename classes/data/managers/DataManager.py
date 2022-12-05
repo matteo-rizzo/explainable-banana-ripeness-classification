4bfe6f16cf9685da0e52b0b370a182cef26ebc45
Matteo Marcuzzo <matteo.marcuzzo.it@gmail.com>
2022-12-05 14:36:58 +0100

    Test

--- classes/data/managers/DataManager.py
+++ classes/data/managers/DataManager.py
@@ -1,14 +1,14 @@
 import os
 import pprint
 from pathlib import Path
-from typing import Dict, Union
+from typing import Dict, Union, Callable, List
 
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import ShuffleSplit
 from termcolor import colored
 from torch.utils.data import DataLoader
-
+from pathlib import Path
 from classes.data.Dataset import Dataset
 from classes.data.loaders.ImageLoader import ImageLoader
 
@@ -16,15 +16,14 @@ from classes.data.loaders.ImageLoader import ImageLoader
 class DataManager:
 
     def __init__(self, data_params: Dict):
-        self.__path_to_images = data_params["dataset"]["paths"]["images"]
-        self.__k = data_params["cv"]["k"]
-        self.__batch_size = data_params["batch_size"]
-        self.__loader = ImageLoader().load
-        self.__split_data = []
-        self.__data = self.__read_data()
+        self._path_to_images: Path = Path(data_params["dataset"]["paths"]["images"])
+        self._k: int = data_params["cv"]["k"]
+        self._batch_size: int = data_params["batch_size"]
+        self._loader: Callable = ImageLoader().load
+        self._split_data: List = []
 
     def get_k(self) -> int:
-        return self.__k
+        return self._k
 
     @staticmethod
     def get_full_dataloader(path_to_data: str, file_names_ok: bool = False):
@@ -49,13 +48,13 @@ class DataManager:
         """ Reloads the data split from saved metadata in CSV format """
         saved_data = pd.read_csv(os.path.join(path_to_metadata, "split_{}.csv".format(seed)),
                                  converters={"train": eval, "val": eval, "test": eval})
-        self.__split_data = saved_data.to_dict("records")
+        self._split_data = saved_data.to_dict("records")
 
     def print_split_info(self, verbose: bool = False):
         """ Shows how the data has been split_data in each fold """
 
         split_info = []
-        for fold_paths in self.__split_data:
+        for fold_paths in self._split_data:
             split_info.append({
                 'train': list(set([item.split("_")[-1] for item in fold_paths['train'][0]])),
                 'val': list(set([item.split("_")[-1] for item in fold_paths['val'][0]])),
@@ -66,7 +65,7 @@ class DataManager:
             print("\n..............................................\n")
             print("Split info overview:\n")
             pp = pprint.PrettyPrinter(compact=True)
-            for fold in range(self.__k):
+            for fold in range(self._k):
                 print(colored(f'fold {fold}: ', 'blue'))
                 pp.pprint(split_info[fold])
                 print('\n')
@@ -75,7 +74,7 @@ class DataManager:
     def load_split(self, fold: int) -> Dict:
         """ Loads the data based on the fold paths """
 
-        fold_paths = self.__split_data[fold]
+        fold_paths = self._split_data[fold]
 
         x_train_paths, y_train = fold_paths['train']
         x_val_paths, y_valid = fold_paths['val']
@@ -88,17 +87,17 @@ class DataManager:
         print("..............................................")
 
         return {
-            'train': DataLoader(Dataset(x_train_paths, y_train, self.__loader),
-                                self.__batch_size, shuffle=True, drop_last=True),
-            'val': DataLoader(Dataset(x_val_paths, y_valid, self.__loader),
-                              self.__batch_size, drop_last=True),
-            'test': DataLoader(Dataset(x_test_paths, y_test, self.__loader),
-                               self.__batch_size, drop_last=True)
+            'train': DataLoader(Dataset(x_train_paths, y_train, self._loader),
+                                self._batch_size, shuffle=True, drop_last=True),
+            'val': DataLoader(Dataset(x_val_paths, y_valid, self._loader),
+                              self._batch_size, drop_last=True),
+            'test': DataLoader(Dataset(x_test_paths, y_test, self._loader),
+                               self._batch_size, drop_last=True)
         }
 
     def save_split_to_file(self, path_to_results: Union[str, Path], seed: int):
         path_to_metadata = os.path.join(path_to_results, "cv_splits")
         path_to_file = os.path.join(path_to_metadata, f"split_{seed}.csv")
         os.makedirs(path_to_metadata, exist_ok=True)
-        pd.DataFrame(self.__split_data).to_csv(path_to_file, index=False)
+        pd.DataFrame(self._split_data).to_csv(path_to_file, index=False)
         print(f" CV split metadata written at {path_to_file}")
