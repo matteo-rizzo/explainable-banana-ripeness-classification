045df9f5ee65ef8d728a6f797a47ce393c060566
alek9z <alessandro.zangari.code@outlook.com>
2022-12-13 12:23:31 +0100

    "Interpretable" DT with cubes

--- scripts/decision_tree.py
+++ scripts/decision_tree.py
@@ -5,6 +5,8 @@ from typing import Dict, List, Tuple, Any
 import numpy as np
 import pandas as pd
 import torch
+from matplotlib import pyplot as plt
+from matplotlib.pyplot import gcf
 from sklearn import tree
 from sklearn.model_selection import train_test_split
 from torchmetrics.functional.classification import multiclass_precision, multiclass_recall, multiclass_f1_score
@@ -86,19 +88,33 @@ def train_dt():
     # fig.set_dpi(1000)
     # plt.show()
 
-    return decision_tree, color_feature_names, num_classes
+    return decision_tree, feature_names, num_classes
 
 
-def merge_rules(per_class_rules: List[List[Tuple[float]]], new_rules: Dict[str, List[float]], class_idx: int,
-                max_val: float):
-    to_update: List[Tuple[float]] = per_class_rules[class_idx]
+def merge_rules(per_class_rules: List[List[List[Tuple[float, float]]]], new_rules: Dict[str, List[Tuple[float, bool]]], class_idx: int, max_val: float, min_val: float) -> None:
+    to_update: List[List[Tuple[float, float]]] = per_class_rules[class_idx]
 
-    color_tuple = tuple([min(new_rules[k], default=max_val) for k in new_rules.keys()])
-    to_update.append(color_tuple)
+    feature_ranges: List[Tuple[float, float]] = list()
+    for feature_name, path in new_rules.items():
+        node_min = min_val
+        node_max = max_val
+        for (node_threshold, sign) in path:
+            if sign is False:
+                # <=
+                if node_threshold < node_max:
+                    node_max = node_threshold
+            else:
+                # >
+                if node_threshold > node_min:
+                    node_min = node_threshold
+
+            feature_ranges.append((node_min, node_max))
+        if not path:
+            feature_ranges.append((node_min, node_max))
+    to_update.append(feature_ranges)
 
 
-def get_leaf_constraints(clf: tree.DecisionTreeClassifier, feature_names: List[Any], num_classes: int) -> List[
-    List[Tuple[float]]]:
+def get_leaf_constraints(clf: tree.DecisionTreeClassifier, feature_names: List[Any], num_classes: int) -> List[List[List[Tuple[float, float]]]]:
     """
     For each target class, return the set of constraints on each feature that lead to that classification
 
@@ -113,36 +129,48 @@ def get_leaf_constraints(clf: tree.DecisionTreeClassifier, feature_names: List[A
     threshold = clf.tree_.threshold
     prediction = clf.tree_.value.argmax(axis=2).reshape(-1)
 
-    per_class_rules: List[List[Tuple[float]]] = [[] for _ in range(num_classes)]
-    rules: Dict[str, List[float]] = {fn: list() for fn in feature_names}
+    # For each class, we want a list of leaf rules leading to that class
+    # Each leaf rule is a List of ranges (min-max) for each feature
+    per_class_rules: List[List[List[Tuple[float, float]]]] = [[] for _ in range(num_classes)]
+    rules: Dict[str, List[Tuple[float, bool]]] = {fn: list() for fn in feature_names}
 
     stack = [(0, rules)]  # start with the root node id (0) and its depth (0)
     while len(stack) > 0:
         # `pop` ensures each node is only visited once
         node_id, rule_path = stack.pop()
 
-        f, t = feature[node_id], threshold[node_id]
+        node_feature, node_threshold = feature[node_id], threshold[node_id]
 
         is_split_node = children_left[node_id] != children_right[node_id]
         if is_split_node:
             # Decision node
-            rule_path[feature_names[f]].append(t)
+
+            ln = copy.deepcopy(rule_path)
+            rn = copy.deepcopy(rule_path)
+
+            ln[feature_names[node_feature]].append((node_threshold, False))  # <=
+            rn[feature_names[node_feature]].append((node_threshold, True))  # >
 
             # Append left and right children and depth to `stack`
-            stack.append((children_right[node_id], copy.deepcopy(rule_path)))
-            stack.append((children_left[node_id], copy.deepcopy(rule_path)))
+            stack.append((children_left[node_id], ln))
+            stack.append((children_right[node_id], rn))
         else:
             # Leaf node
             if feature_names == ["u", "v"]:
                 # U and V values should be in [-.5, .5]
                 max_val = .5
+                min_val = -.5
             else:
+                # RGB in [0, 1]
                 max_val = 1.0
-            merge_rules(per_class_rules, rule_path, prediction[node_id], max_val=max_val)
+                min_val = .0
+            merge_rules(per_class_rules, rule_path, prediction[node_id], max_val=max_val, min_val=min_val)
 
     assert sum([len(a) for a in per_class_rules]) == clf.tree_.n_leaves, "Wrong number of leaves and outputs"
     return per_class_rules
 
 
 if __name__ == "__main__":
-    train_dt()
+    dt, f, c = train_dt()
+    leaves = get_leaf_constraints(dt, f, c)
+    pass
--- scripts/interpret_dt.py
+++ scripts/interpret_dt.py
@@ -1,8 +1,8 @@
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 import numpy as np
 from matplotlib import pyplot as plt
-from skimage.color import yuv2rgb
+from matplotlib.axes import Axes
 from sklearn.tree import DecisionTreeClassifier
 
 from scripts.decision_tree import get_leaf_constraints, train_dt
@@ -17,58 +17,52 @@ def explode(data):
     return data_e
 
 
-def plot_rgb_explanation(rules: List[Tuple], max_values: int = None):
+def plot_rgb_explanation(rule: List[Tuple[float, float]], max_plot_components: int = None, axes: Axes = None, title: Optional[str] = None) -> Axes:
     """
     Produce Voxel plot
 
-    :param rules: rules extracted from DT
-    :param max_values: number of cubes to plot (tradeoff for speed, 256 is too much)
+    :param rule: rule extracted from DT, a tuple of min and max value for each feature
+    :param max_plot_components: number of cubes to plot (tradeoff for speed, 256 is too much)
     """
     res_max = 256
 
-    if max_values is not None:
-        bins = np.linspace(start=0.0, stop=1.0, num=max_values + 1, endpoint=True)
-        np_rules = np.clip(np.digitize(rules, bins) - 1, a_min=0, a_max=max_values - 1)  # (rules_n, 3), values in [0, max_value)
-        res_max = max_values
+    if max_plot_components is not None:
+        bins = np.linspace(start=0.0, stop=1.0, num=max_plot_components + 1, endpoint=True)
+        np_rule = np.clip(np.digitize(rule, bins) - 1, a_min=0, a_max=max_plot_components - 1)  # (feature_n, 2), values in [0, max_value)
+        res_max = max_plot_components
         # Average RGB color in each consecutive interval
         indices = np.array([[i, i + 1] for i in range(bins.shape[0] - 1)])
         colors = bins[indices].mean(axis=-1)  # max_value colors
         del bins
         del indices
     else:
-        int_rgbs = [tuple([int(c * (res_max - 1)) for c in components]) for components in rules]
-        np_rules = np.array(int_rgbs)  # (rules_n, 3)
+        int_rgbs = [[(int(b * (res_max - 1)), int(t * (res_max - 1))) for (b, t) in components] for components in rule]
+        np_rule = np.array(int_rgbs)  # (feature_n, 2)
 
     # Remove duplicated RGB tuples
-    np_rules = np.unique(np_rules, axis=0)
+    # np_rules = np.unique(np_rules, axis=0)
 
     # Expanded set of rules
-    # TODO: check this and rule generation
-    ext_rules = list()
-    for rule in np_rules:
-        # rule is a triple RGB
-        r = np.arange(rule[0] + 1, dtype=int)
-        g = np.arange(rule[1] + 1, dtype=int)
-        b = np.arange(rule[2] + 1, dtype=int)
-        # Compute all possible combinations of RGB allowed values
-        combination = np.array(np.meshgrid(r, g, b)).T.reshape(-1, 3)
-        ext_rules.append(combination)
-    ext_rules = np.concatenate(ext_rules, axis=0)
-    np_rules = np.unique(ext_rules, axis=0)
+    r = np.arange(start=np_rule[0, 0], stop=np_rule[0, 1] + 1, dtype=int)
+    g = np.arange(start=np_rule[1, 0], stop=np_rule[1, 1] + 1, dtype=int)
+    b = np.arange(start=np_rule[2, 0], stop=np_rule[2, 1] + 1, dtype=int)
+    # Compute all possible combinations of RGB allowed values
+    combination = np.array(np.meshgrid(r, g, b)).T.reshape(-1, 3)
+    np_rule = np.unique(combination, axis=0)
 
     # create voxels cubes
     n_voxels = np.zeros((res_max, res_max, res_max), dtype=bool)
-    n_voxels[tuple(np_rules.T)] = True
+    n_voxels[tuple(np_rule.T)] = True
 
-    if max_values is None:
-        colors = np_rules
+    if max_plot_components is None:
+        colors = np_rule
     else:
-        colors = colors[np_rules]
+        colors = colors[np_rule]
 
     facecolors = np.zeros((res_max, res_max, res_max, 4))
     # Add suitable alpha channel, 0 for black, .6 for colors (semi-tranparent)
     colors = np.concatenate([colors, np.full((colors.shape[0], 1), .6)], axis=1)  # (n_rules, 4)
-    facecolors[tuple(np_rules.T)] = colors
+    facecolors[tuple(np_rule.T)] = colors
 
     filled = np.ones(n_voxels.shape)
     # upscale the above voxel image, leaving gaps
@@ -89,34 +83,42 @@ def plot_rgb_explanation(rules: List[Tuple], max_values: int = None):
     # f = _create_voxel_mesh_figure(a, a, fcolors_2, "test")
     # f.show()
 
-    ax = plt.figure(figsize=(15, 15), dpi=300).add_subplot(projection="3d")
+    ax = axes
+    if ax is None:
+        ax = plt.figure(figsize=(15, 15), dpi=300).add_subplot(projection="3d")
     ax.voxels(x, y, z, filled, facecolors=facecolors, edgecolors=facecolors)
     ax.set_aspect("equal")
-
-    plt.show()
+    ax.axes.set_xlim3d(left=0.000001, right=max_plot_components + 0.9999999)
+    ax.axes.set_ylim3d(bottom=0.000001, top=max_plot_components + 0.9999999)
+    ax.axes.set_zlim3d(bottom=0.000001, top=max_plot_components + 0.9999999)
+    ax.set_xlabel("R")
+    ax.set_ylabel("G")
+    ax.set_zlabel("B")
+    if title:
+        ax.set_title(title)
+    return ax
 
 
 def interpret_decision_tree(decision_tree: DecisionTreeClassifier, feature_names: List, num_classes: int):
-    rules_extracted: list[list[tuple[float]]] = get_leaf_constraints(decision_tree, feature_names=feature_names, num_classes=num_classes)
+    rules_extracted: List[List[List[Tuple[float, float]]]] = get_leaf_constraints(decision_tree, feature_names=feature_names, num_classes=num_classes)
 
     if feature_names == ["u", "v"]:
-        rules_extracted = [[tuple((yuv2rgb((.65, *r)) * 255).round().astype(int)) for r in regions] for regions in rules_extracted]
-
-    # plot_hull_section_colours(hull, section_colours="RGB", section_opacity=1.0)
-    # cat_1 = np.array(rules_extracted[0])
-    # opacity = np.array([1.0] * len(cat_1)).reshape(-1, 1)
-
-    # plot_RGB_colourspace_section(
-    #     "sRGB", section_colours=cat_1, section_opacity=opacity
-    # )
+        raise ValueError("Explanation with YUV unsupported at the moment.")
+        # rules_extracted = [[[yuv2rgb(()) for (min_f, max_f) in r] for r in regions] for regions in rules_extracted]
 
     return rules_extracted
 
 
 def main():
     dt, feature_names, classes = train_dt()
-    rules = interpret_decision_tree(dt, feature_names, classes)
-    plot_rgb_explanation(rules[1], max_values=16)
+    rules: List[List[List[Tuple[float, float]]]] = interpret_decision_tree(dt, feature_names, classes)
+    for ripeness, class_rules in enumerate(rules):
+        fig = None
+        for leaf_rule in class_rules:
+            fig = plot_rgb_explanation(leaf_rule, max_plot_components=16, axes=fig)
+        fig.set_title(f"RGB area for ripeness value {ripeness}")
+        plt.tight_layout()
+        plt.show()
 
 
 if __name__ == "__main__":
