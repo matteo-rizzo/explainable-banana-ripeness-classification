64c54cc3938a2af42b44df8ea19b7c8bb2eb2ed3
alek9z <alessandro.zangari.code@outlook.com>
2022-12-14 13:06:44 +0100

    Slightly more efficient plots, but still problematic for 128 cubes

--- scripts/interpret_dt.py
+++ scripts/interpret_dt.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Optional
+from typing import List, Tuple
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -37,17 +37,49 @@ def create_bins_range(value_range: Tuple[float, float], rule: Tuple[float, float
     return binned_rule, colors
 
 
-def plot_rgb_explanation(rule: List[Tuple[float, float]],
-                         r_range: Tuple[float, float], g_range: Tuple[float, float], b_range: Tuple[float, float],
-                         cubes_n: int = 16, axes: Optional[Axes] = None,
-                         title: Optional[str] = None) -> Axes:
+def create_plot(n_voxels, face_colors) -> Axes:
     """
-    Produce Voxel plot
+    Create a voxel plot with specified cubes and colors
+
+    :param n_voxels: bool 3D array with cubes to be filled with color
+    :param face_colors: 4D RGBA colors for each cube
+    :return: matplotlib axes of the produced plot
+    """
+    cubes_n = n_voxels.shape[0]
+    filled = np.ones(n_voxels.shape)
+    # Upscale the above voxel image, leaving gaps
+    filled = explode(filled)
+    face_colors = explode(face_colors)
+
+    # Shrink the gaps
+    x, y, z = np.indices(np.array(filled.shape) + 1).astype(float) // 2
+    x[0::2, :, :] += 0.05
+    y[:, 0::2, :] += 0.05
+    z[:, :, 0::2] += 0.05
+    x[1::2, :, :] += 0.95
+    y[:, 1::2, :] += 0.95
+    z[:, :, 1::2] += 0.95
+
+    ax = plt.figure(figsize=(10, 10), dpi=200).add_subplot(projection="3d")
+    ax.voxels(x, y, z, filled, facecolors=face_colors, edgecolors=face_colors)
+    ax.set_aspect("equal")
+    ax.axes.set_xlim3d(left=0.000001, right=cubes_n + 0.9999999)
+    ax.axes.set_ylim3d(bottom=0.000001, top=cubes_n + 0.9999999)
+    ax.axes.set_zlim3d(bottom=0.000001, top=cubes_n + 0.9999999)
+    ax.set_xlabel("R", fontsize=18)
+    ax.set_ylabel("G", fontsize=18)
+    ax.set_zlabel("B", fontsize=18)
+    return ax
+
+
+def plot_rgb_explanation(rule: List[Tuple[float, float]], cubes_n: int, alpha: float,
+                         r_range: Tuple[float, float], g_range: Tuple[float, float], b_range: Tuple[float, float]) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Produce voxel plot "filled" and "facecolors" parameters for a single decision rule.
 
     :param rule: rule extracted from DT, a tuple of min and max value for each feature
     :param cubes_n: number of cubes to plot (tradeoff for speed, 256 is too much)
-    :param axes: matplotlib axes where to plot image
-    :param title: optional title for figure
+    :param alpha: value for alpha channel of all cubes
     :param r_range: range to show for R channel
     :param g_range: range to show for G channel
     :param b_range: range to show for B channel
@@ -87,37 +119,10 @@ def plot_rgb_explanation(rule: List[Tuple[float, float]],
 
     facecolors = np.zeros((cubes_n, cubes_n, cubes_n, 4))
     # Add suitable alpha channel, 0 for black, .6 for colors (semi-transparent)
-    colors = np.concatenate([colors, np.full((colors.shape[0], 1), .6)], axis=1)  # (n_rules, 4)
+    colors = np.concatenate([colors, np.full((colors.shape[0], 1), alpha)], axis=1)  # (n_rules, 4)
     facecolors[tuple(np_rule.T)] = colors
 
-    filled = np.ones(n_voxels.shape)
-    # upscale the above voxel image, leaving gaps
-    filled = explode(filled)
-    facecolors = explode(facecolors)
-
-    # Shrink the gaps
-    x, y, z = np.indices(np.array(filled.shape) + 1).astype(float) // 2
-    x[0::2, :, :] += 0.05
-    y[:, 0::2, :] += 0.05
-    z[:, :, 0::2] += 0.05
-    x[1::2, :, :] += 0.95
-    y[:, 1::2, :] += 0.95
-    z[:, :, 1::2] += 0.95
-
-    ax = axes
-    if ax is None:
-        ax = plt.figure(figsize=(10, 10), dpi=200).add_subplot(projection="3d")
-    ax.voxels(x, y, z, filled, facecolors=facecolors, edgecolors=facecolors)
-    ax.set_aspect("equal")
-    ax.axes.set_xlim3d(left=0.000001, right=cubes_n + 0.9999999)
-    ax.axes.set_ylim3d(bottom=0.000001, top=cubes_n + 0.9999999)
-    ax.axes.set_zlim3d(bottom=0.000001, top=cubes_n + 0.9999999)
-    ax.set_xlabel("R", fontsize=18)
-    ax.set_ylabel("G", fontsize=18)
-    ax.set_zlabel("B", fontsize=18)
-    if title:
-        ax.set_title(title, fontsize=18)
-    return ax
+    return n_voxels, facecolors
 
 
 def interpret_decision_tree(decision_tree: DecisionTreeClassifier, feature_names: List, num_classes: int):
@@ -131,20 +136,34 @@ def interpret_decision_tree(decision_tree: DecisionTreeClassifier, feature_names
 
 
 def main():
+    # Range of colors to plot
     r_range = (0.41, 0.65)
     g_range = (0.35, 0.70)
     b_range = (0.13, 0.40)
+    # Number of cubes for each axis. Each cube represent a different color.
+    cube_n = 80
+    # Transparency of the cubes. 0 is completely transparent, 1 is opaque.
+    alpha_channel = .3
 
     dt, feature_names, classes = train_dt()
     rules: List[List[List[Tuple[float, float]]]] = interpret_decision_tree(dt, feature_names, classes)
     for ripeness, class_rules in enumerate(rules):
-        fig = None
+        voxels, face_colors = np.zeros((cube_n, cube_n, cube_n), dtype=bool), np.zeros((cube_n, cube_n, cube_n, 4), dtype=float)
+        # Accumulate voxels representation from each leaf in a single vector
         for leaf_rule in class_rules:
-            fig = plot_rgb_explanation(leaf_rule, cubes_n=20, axes=fig, r_range=r_range, g_range=g_range, b_range=b_range)
-        fig.set_title(f"RGB area for ripeness value {ripeness}")
+            voxels_leaf, face_colors_leaf = plot_rgb_explanation(leaf_rule, cubes_n=cube_n, r_range=r_range, g_range=g_range, b_range=b_range, alpha=alpha_channel)
+            # Union of cubes from different leaves
+            voxels |= voxels_leaf
+            assert np.all(face_colors.shape == face_colors_leaf.shape), "Wrong shapes to unite"
+            face_colors = np.where(face_colors > 0, face_colors, face_colors_leaf)
+        fig = create_plot(voxels, face_colors)
+        fig.set_title(f"RGB area for ripeness value {ripeness}", fontsize=18)
         plt.tight_layout()
         # plt.show()
         plt.savefig(f"{ripeness}_rgb.png")
+        fig.clear()
+        plt.clf()
+        plt.close()
 
 
 if __name__ == "__main__":
