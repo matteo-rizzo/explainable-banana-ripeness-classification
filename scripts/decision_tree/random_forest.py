885e5504b8c2fcc2a6b9415433b77aa15cd1a408
Matteo Marcuzzo <matteo.marcuzzo.it@gmail.com>
2022-12-16 13:46:02 +0100

    Error analysis

--- scripts/decision_tree/random_forest.py
+++ scripts/decision_tree/random_forest.py
@@ -5,9 +5,10 @@ import numpy as np
 import pandas as pd
 import torch
 from sklearn import tree
-from sklearn.model_selection import train_test_split
-from torchmetrics.functional.classification import multiclass_precision, multiclass_recall, multiclass_f1_score
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from torchmetrics.functional.classification import multiclass_precision, multiclass_recall, multiclass_f1_score
+
 from functional.yaml_manager import load_yaml
 
 """
@@ -45,7 +46,7 @@ def prepare_features(train_config):
     return x, y, color_feature_names
 
 
-def train_dt():
+def train_forest():
     # Load configuration
     train_config: Dict = load_yaml("params/networks/random_forest.yml")
     num_classes: int = train_config["output_size"]
@@ -95,6 +96,59 @@ def train_dt():
     return random_forest, color_feature_names, num_classes
 
 
+def grid_search_best_params():
+    # Load configuration
+    train_config: Dict = load_yaml("params/networks/random_forest.yml")
+    num_classes: int = train_config["output_size"]
+    num_rand_states: int = train_config["num_seeds"]
+    test_size: float = train_config["test_size"]
+
+    # Prepare features and target
+    x, y, color_feature_names = prepare_features(train_config)
+    # Initiate training
+    avg_metrics: Dict[str, List] = {"accuracy": [], "precision": [], "recall": [], "f1": []}
+
+    # Per-seed training
+    for rs in range(num_rand_states):
+        # Prepare splits
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=rs)
+        y_train, y_test = y_train["y"].tolist(), y_test["y"].tolist()
+
+        # Setup and train classifier
+        grid_clf = GridSearchCV(RandomForestClassifier(), n_jobs=-1,
+                                param_grid=train_config["grid_search_params"], verbose=10, )
+        grid_clf.fit(x_train, y_train)
+        y_pred = grid_clf.predict(x_test).tolist()
+
+        # Calculate metrics
+        y_pred, y_test = torch.Tensor(y_pred), torch.Tensor(y_test)
+        metrics = {
+            "accuracy": sum([1 for a, b in zip(y_test, y_pred) if a == b]) / len(y_pred),
+            "precision": float(multiclass_precision(y_pred, y_test, num_classes=num_classes)),
+            "recall": float(multiclass_recall(y_pred, y_test, num_classes=num_classes)),
+            "f1": float(multiclass_f1_score(y_pred, y_test, num_classes=num_classes))
+        }
+
+        # Print results
+        print(f"Random Seed {rs} - Test Metrics:")
+        for metric, value in metrics.items():
+            print(f"\t {metric} - {''.join(['.'] * (15 - len(metric)))} : {value:.4f}")
+
+        avg_metrics["accuracy"].append(metrics["accuracy"])
+        avg_metrics["precision"].append(metrics["precision"])
+        avg_metrics["recall"].append(metrics["recall"])
+        avg_metrics["f1"].append(metrics["f1"])
+
+    print("-----------------------------------------------------------")
+    print(f"Average Test Metrics Over {num_rand_states} Random Seeds:")
+    for metric, value in avg_metrics.items():
+        print(f"\t {metric} - {''.join(['.'] * (15 - len(metric)))} : {np.mean(value):.4f} ({np.std(value):.4f})")
+
+    print("-----------------------------------------------------------")
+    print(grid_clf.best_params_)
+
+
 if __name__ == "__main__":
-    dt, f, c = train_dt()
-    pass
+    # dt, f, c = train_forest()
+    grid_search_best_params()
+    # pass
